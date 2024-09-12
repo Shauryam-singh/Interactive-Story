61926a189a4a6d5118e4cc162c578ad3fd8e718b
Shauryam-singh <shauryamsingh9@gmail.com>
2024-09-12 19:31:41 +0530

    Remoded

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = document.getElementById('fire-sound');
-    audio.play().catch(function(error) {
-        console.log('Autoplay was prevented:', error);
-    });
-});
